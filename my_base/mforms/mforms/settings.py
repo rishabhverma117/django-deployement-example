d34cb200ec42ce538ba4c39ed5672cf51c86c97d
rishabhverma117 <68354602+rishabhverma117@users.noreply.github.com>
2021-05-24 14:17:42 +0530

    Update settings.py

--- my_base/mforms/mforms/settings.py
+++ my_base/mforms/mforms/settings.py
@@ -14,9 +14,9 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
-STATIC_DIR = os.path.join(BASE_DIR,"static")
-MEDIA_DIR = os.path.join(BASE_DIR,"media")
+TEMPLATES_DIR = BASE_DIR.joinpath('templates')
+STATIC_DIR = BASE_DIR.joinpath("static")
+MEDIA_DIR = BASE_DIR.joinpath("media")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
